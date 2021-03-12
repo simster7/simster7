46251bb4e25a0985bc6cf7976b440acbd7bf788a
Simon Behar <simbeh7@gmail.com>
2021-03-12 08:22:25 -0800

    first commit
    Signed-off-by: Simon Behar <simbeh7@gmail.com>

---
+++ scrape.py
@@ -0,0 +1,14 @@
+import shutil
+import requests
+
+shields = {
+    "github": "https://img.shields.io/github/followers/simster7.svg?label=GitHub&style=social",
+    "twitter": "https://img.shields.io/twitter/follow/simster7?label=Twitter&style=social",
+    "linkedin": "https://img.shields.io/badge/LinkedIn--_.svg?style=social&logo=linkedin",
+}
+
+for shield, url in shields.items():
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open("img/" + shield + ".svg", 'wb') as f:
+            f.write(response.content)
