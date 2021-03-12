7426d39312c0bd9468e3b812309b2b56b09487aa
Simon Behar <simbeh7@gmail.com>
2021-03-12 09:21:55 -0800

    email
    Signed-off-by: Simon Behar <simbeh7@gmail.com>

--- README.md
+++ README.md
@@ -7,6 +7,7 @@
 </p>
 
 <p align="center">
+	<a href="mailto:simbeh7@gmail.com""><img src="img/gmail.svg" alt="Email"></a>
 	<a href="https://www.linkedin.com/in/sbehar"><img src="img/linkedin.svg" alt="LinkedIn"></a>
 	<a href="https://github.com/simster7"><img src="img/github.svg" alt="GitHub"></a>
 	<a href="https://twitter.com/simster7"><img src="img/twitter.svg" alt="Twitter"></a>
--- scrape.py
+++ scrape.py
@@ -5,6 +5,7 @@ shields = {
     "github": "https://img.shields.io/github/followers/simster7.svg?label=GitHub&style=social",
     "twitter": "https://img.shields.io/twitter/follow/simster7?label=Twitter&style=social",
     "linkedin": "https://img.shields.io/badge/LinkedIn--_.svg?style=social&logo=linkedin",
+    "gmail": "https://img.shields.io/badge/Email-simbeh7@gmail.com-_.svg?style=social&logo=gmail"
 }
 
 for shield, url in shields.items():
